9457da697201f8719e53156447c14a6da7edb30e
Esen Arykbaev <esen.arykbaev@dteam.dev>
2021-01-23 02:49:47 +0600

    feature/fix-android

--- android/app/src/main/java/com/lmaapp/MainApplication.java
+++ android/app/src/main/java/com/lmaapp/MainApplication.java
@@ -5,10 +5,8 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -31,8 +29,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new VectorIconsPackage());
-          packages.add(new ReactNativePushNotificationPackage());
           return packages;
         }
 
